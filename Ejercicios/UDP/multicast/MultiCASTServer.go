0e4c391ca092d0fb1c45cc67d71c34d0f9af1385
Jack Cloudman <maylo360xd@gmail.com>
2019-08-29 09:07:12 -0500

    organize example files

---
+++ Ejercicios/UDP/multicast/MultiCASTServer.go
@@ -0,0 +1,44 @@
+// Ejemplo paa servidor multicast
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/dmichael/go-multicast/multicast"
+	"github.com/urfave/cli"
+)
+
+const (
+	defaultMulticastAddress = "229.1.2.3:9999"
+)
+
+func main() {
+	app := cli.NewApp()
+
+	app.Action = func(c *cli.Context) error {
+		address := c.Args().Get(0)
+		if address == "" {
+			address = defaultMulticastAddress
+		}
+		fmt.Printf("Broadcasting to %s\n", address)
+		ping(address)
+		return nil
+	}
+
+	app.Run(os.Args)
+}
+
+func ping(addr string) {
+	conn, err := multicast.NewBroadcaster(addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn.Write([]byte("hello, world\n"))
+		time.Sleep(1 * time.Second)
+	}
+}
