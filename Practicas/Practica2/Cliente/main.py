b41e213d4b51fa4e5d2add375ff71ff69ac71c0e
OscarArmas <saga_cth@hotmail.com>
2019-09-01 15:01:49 -0500

    mostrar_lista_preview

---
+++ Practicas/Practica2/Cliente/main.py
@@ -0,0 +1,100 @@
+from PyQt5 import QtWidgets, uic
+import sys
+from PyQt5.QtCore import QRegExp
+from PyQt5.QtGui import QRegExpValidator, QPixmap
+from PyQt5.QtWidgets import QMessageBox, QFileDialog, QListWidget, QWidget, QVBoxLayout, QHBoxLayout,QLabel, QListWidgetItem
+import socket
+import shutil
+from os.path import basename
+from os import getcwd,stat,remove
+import json,time
+from utils import QCustomQWidget
+
+
+
+class Ui(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Ui, self).__init__() # Call the inherited classes __init__ method
+        uic.loadUi('gui.ui', self) # Load the .ui file
+        # Validator
+        reg_host = QRegExp("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$")
+        input_validator = QRegExpValidator(reg_host, self.HOST)
+        self.HOST.setValidator(input_validator)
+
+        reg_port = QRegExp("^([0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$")
+        input_validator = QRegExpValidator(reg_port, self.PORT)
+        self.PORT.setValidator(input_validator)
+
+        self.BTN_CONN.clicked.connect(self.connect)
+        #self.BTN_SFILE.clicked.connect(self.openFile)
+        #self.BTN_SFOLDER.clicked.connect(self.openFolder)
+        #self.BTN_UPLOAD.clicked.connect(self.upload)
+        #self.BTN_LIST.clicked.connect(self.path_list)
+        self.send = None
+        self.show() # Show the GUI
+    def connect(self):
+        h = self.HOST.text()
+        p = self.PORT.text()
+        if h==p=="":
+            return
+        try:
+            self.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+            self.s.connect((h,int(p)))
+        except Exception as e:
+            self.show_message("Error!","No se ha podido establecer comunicación")
+            print(e)
+            return
+        # Buttons
+        self.BTN_CONN.setEnabled(False)
+        self.HOST.setEnabled(False)
+        self.PORT.setEnabled(False)
+        self.STATUS.setChecked(True)
+        #self.BTN_SFILE.setEnabled(True)
+        #self.BTN_SFOLDER.setEnabled(True)
+        self.show_message("Exito!","Conexion establecida")
+
+        ################################################################
+
+        datas =self.getCatalogo(self.s)
+        self.myQListWidget = QListWidget(self)
+        for articulos in datas['articulos']:
+            # Create QCustomQWidget
+            myQCustomQWidget = QCustomQWidget()
+            myQCustomQWidget.setTextUp(articulos['nombre'])
+            myQCustomQWidget.setTextDown(str(articulos['precio']))
+            myQCustomQWidget.setIcon('icon.png')
+            # Create QListWidgetItem
+            myQListWidgetItem = QListWidgetItem(self.myQListWidget)
+            # Set size hint
+            myQListWidgetItem.setSizeHint(myQCustomQWidget.sizeHint())
+            # Add QListWidgetItem into QListWidget
+            self.myQListWidget.addItem(myQListWidgetItem)
+            self.myQListWidget.setItemWidget(myQListWidgetItem, myQCustomQWidget)
+            self.setCentralWidget(self.myQListWidget)
+
+
+    def show_message(self,title,m):
+        QMessageBox.about(self, title,m)
+
+
+    def recvall(self, sock):
+        BUFF_SIZE = 4096 # 4 KiB
+        data = b''
+        while True:
+            part = sock.recv(BUFF_SIZE)
+            data += part
+            if len(part) < BUFF_SIZE:
+                # either 0 or end of data
+                break
+        return json.loads(data.decode())
+
+    def getCatalogo(self, socket):
+        command = '{"command":"getCatalogo"}'
+        socket.sendall(command.encode())
+        return self.recvall(socket)
+
+
+
+app = QtWidgets.QApplication(sys.argv)
+window = Ui()
+app.exec_()
--- Practicas/Practica2/Cliente/utils.py
+++ Practicas/Practica2/Cliente/utils.py
@@ -1,3 +1,42 @@
+from PyQt5 import QtWidgets, uic
+from PyQt5.QtGui import QRegExpValidator, QPixmap
+from PyQt5.QtWidgets import QMessageBox, QFileDialog, QListWidget, QWidget, QVBoxLayout, QHBoxLayout,QLabel, QListWidgetItem
+
+
+class QCustomQWidget(QWidget):
+
+    def __init__ (self, parent = None):
+        super(QCustomQWidget, self).__init__(parent)
+
+        self.setWindowTitle("My Awesome App")
+        self.textQVBoxLayout = QVBoxLayout()
+        self.textUpQLabel    = QLabel()
+        self.textDownQLabel  = QLabel()
+        self.textQVBoxLayout.addWidget(self.textUpQLabel)
+        self.textQVBoxLayout.addWidget(self.textDownQLabel)
+        self.allQHBoxLayout  = QHBoxLayout()
+        self.iconQLabel      = QLabel()
+        self.allQHBoxLayout.addWidget(self.iconQLabel, 0)
+        self.allQHBoxLayout.addLayout(self.textQVBoxLayout, 1)
+        self.setLayout(self.allQHBoxLayout)
+        # setStyleSheet
+        self.textUpQLabel.setStyleSheet('''
+        color: rgb(0, 0, 255);
+        ''')
+        self.textDownQLabel.setStyleSheet('''
+        color: rgb(255, 0, 0);
+        ''')
+
+    def setTextUp(self, text):
+            self.textUpQLabel.setText(text)
+
+    def setTextDown (self, text):
+            self.textDownQLabel.setText(text)
+
+    def setIcon (self, imagePath):
+            self.iconQLabel.setPixmap(QPixmap(imagePath))
+
+
 #Con esta funcion se obtiene todo el json
 def recvall(sock):
     BUFF_SIZE = 4096 # 4 KiB
